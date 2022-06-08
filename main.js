6bc2b210ffebce61109059b95c255f7ae6897ed2
Hazyautumnrain <renablehots@gmail.com>
2022-06-08 04:23:08 +0400

    animated icon

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const swup = new Swup();
 var count = 0;
 var timer = setInterval(animateTag, 450);
 var tag = document.getElementById("tagimage");
+var icon = document.getElementById('iconlink');
 
 function animateTag() {
 	if (count == 8) count = 0;
 	tag.src = `tag-images/${count}.png`;
+	iconlink.setAttribute("href", `icon-images/${count}.png`);
 	count++;
+	
 }
